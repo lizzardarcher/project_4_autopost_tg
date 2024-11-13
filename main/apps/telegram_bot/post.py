2c223d9247a8076ad44acba7631c69f159c03621
ansel <populator@live.com>
2024-11-13 11:13:32 +0500

    development

--- main/apps/telegram_bot/post.py
+++ main/apps/telegram_bot/post.py
@@ -18,11 +18,7 @@ def post():
     # bots = DataBase.get_bots()
     bots = Bot.objects.all()
     for b in bots:
-        # print(b)
-
-        # token = b[0]
         bot = TeleBot(token=b.token)
-        # post_bot = DataBase.get_bot()
 
         start_date_bot = b.start_date  # дата старта бота
 
@@ -32,73 +28,64 @@ def post():
 
         post_bot_day = b.day  # день постинга у бота
 
-        bot_id = b.id
-
-        # print(start_date_bot, is_start_date_bot, is_bot_active, post_bot_day)
-        # Если бот активен и дата старта совпадает с сегодняшней
-
         if is_start_date_bot and is_bot_active:
-            # print(is_start_date_bot, is_bot_active)
-            # chats = DataBase.get_chats_for_post_by_bot(bot_id)  # Все чаты
             chats = Chat.objects.filter(bot=b) # Все чаты
 
             # Посты. Неотправленные и у которых день постинга совпадает с днём у бота
-            # messages = DataBase.get_posts_by_bot_id((False, post_bot_day, bot_id))
             messages = Post.objects.filter(is_sent=False, day=post_bot_day, bot=b)
 
             # Опрос. Неотправленные и у которых день постинга совпадает с днём у бота
-            # polls = DataBase.get_polls_by_bot_id((False, post_bot_day, bot_id))
             polls = Poll.objects.filter(is_sent=False, day=post_bot_day, bot=b)
 
             m_id = []
             poll_id = []
             for chat in chats:  # По всем чатам
                 chat_reference = chat.reference
-                chat_day = chat.day
-                # print(chat)
                 try:
                     chat_num_id = chat.chat_id
                 except:
                     chat_num_id = chat_reference
 
-                # if chat_day <= post_bot_day:
                 for message in messages:  # По всем одобренным сообщениям
                     message_day = message.day
                     text = message.text
                     mediafile = message.media_file.name
                     post_time = message.post_time
-                    post_time_for_schedule = message.sched_datetime
                     message_id = message.id
                     post_time_init = datetime.strptime(post_time.__str__(), '%H:%M:%S').strftime('%H:%M')
                     server_time = datetime.strftime(datetime.now(), '%H:%M')
                     server_time_plus = datetime.strftime(datetime.now() + timedelta(minutes=3), '%H:%M')
                     # if datetime.strptime(post_time, '%H:%M:%S').strftime('%H:%M') == datetime.now().strftime('%H:%M') and message_day == post_bot_day:
-                    # print(post_time_init, datetime.now().strftime('%H:%M'), server_time_plus)
-                    if post_time_init >= datetime.now().strftime(
-                            '%H:%M') and post_time_init < server_time_plus and message_day == post_bot_day:
+                    # print(f"[Post time : {post_time_init}] [Time now() {datetime.now().strftime('%H:%M')}] [Time now() + {server_time_plus}]")
+                    # print(f"[Start time : {post_time_init >= datetime.now().strftime('%H:%M')}]")
+                    # print(f"[Time start < server_time_plus : {post_time_init < server_time_plus}]")
+                    # print(f"[Message day`n bot_day  {message_day}] / [{post_bot_day}] [{message_day == post_bot_day}]")
+
+                    if post_time_init >= datetime.now().strftime('%H:%M') and post_time_init < server_time_plus and message_day == post_bot_day:
                         try:
-                            # print('Posting accepted', message_id)
+                            print('Posting accepted', message_id)
                             if 'jpg' in mediafile or 'jpeg' in mediafile:
                                 bot.send_photo(chat_id=chat_num_id, photo=open(MEDIA_ROOT + mediafile, 'rb'),
                                                caption=text,
                                                parse_mode='HTML')
-                                # print('Photo sent')
+                                print('Photo sent')
                             elif 'mp3' in mediafile:
                                 bot.send_audio(chat_id=chat_num_id, audio=open(MEDIA_ROOT + mediafile, 'rb'),
                                                caption=text,
                                                parse_mode='HTML')
-                                # print('audio sent')
+                                print('audio sent')
                             elif 'mp4' in mediafile or 'mpeg' in mediafile or 'avi' in mediafile or 'mkv' in mediafile:
                                 bot.send_video(chat_id=chat_num_id, video=open(MEDIA_ROOT + mediafile, 'rb'),
                                                caption=text,
                                                parse_mode='HTML')
-                                # print('video sent')
+                                print('video sent')
                             else:
                                 bot.send_message(chat_id=chat_num_id, text=text, parse_mode='HTML')
-                                # print('text sent')
+                                print('text sent')
                             m_id.append(message_id)
                             chat.error = ''
                             chat.save()
+                            Post.objects.filter(id=message_id).update(is_sent=True)
                             # DataBase.update_chat_error(('', chat_reference.replace('@', 'https://t.me/')))
                         except:
                             print(traceback.format_exc())
@@ -148,6 +135,7 @@ def post():
                             poll_id.append(message_id)
                             chat.error = ''
                             chat.save()
+                            Poll.objects.filter(id=message_id).update(is_sent=True)
                             # DataBase.update_chat_error(('', chat_reference.replace('@', 'https://t.me/')))
                         except:
                             if '403' in traceback.format_exc():
@@ -155,28 +143,36 @@ def post():
                                 chat.save()
                                 # DataBase.update_chat_error(('Ошибка 403 Бот не являестся администратором группы(канала)', chat_reference.replace('@', 'https://t.me/')))
             # print('m_id', m_id)
-            for _id in m_id:
-                Post.objects.filter(id=_id).update(is_sent=True)
-                # DataBase.update_message_is_sent((True, _id))
-            for _id in poll_id:
-                Poll.objects.filter(id=_id).update(is_sent=True)
+            # for _id in m_id:
+            #     Post.objects.filter(id=_id).update(is_sent=True)
+            #     # DataBase.update_message_is_sent((True, _id))
+            # for _id in poll_id:
+            #     Poll.objects.filter(id=_id).update(is_sent=True)
                 # DataBase.update_poll_is_sent((True, _id))
 
             # Если нет постов и опросов, то сдвигаемся к следующей дате
+
             # messages = DataBase.get_posts((False, post_bot_day))
+
             messages = Post.objects.filter(is_sent=False, day=post_bot_day)
+
             # polls = DataBase.get_posts((False, post_bot_day))
+
             polls = Post.objects.filter(is_sent=False, day=post_bot_day)
 
             # print(messages, polls)
+
             if not messages and not polls:
                 new_start_date_bot = str(datetime.strptime(start_date_bot, '%Y-%m-%d')
                                          + timedelta(days=1)).split(' ')[0]
                 new_post_bot_day = post_bot_day + 1
+
                 #  Обновляем день у бота +1
+
                 b.day = new_post_bot_day
                 b.start_date = new_start_date_bot
                 b.save()
+
                 # DataBase.update_bot_day((new_start_date_bot, new_post_bot_day, bot_id))
                 #  Обновляем день у чатов, у которых день такой же как у бота +1
                 # DataBase.update_chat_day_all((new_post_bot_day, post_bot_day))
@@ -185,9 +181,10 @@ def post():
 if __name__ == '__main__':
     try:
         while True:
+            # print('[PENDING] [...]')
             post()
             sleep(5)
-            sys.exit(1)
+            # sys.exit(1)
     except KeyboardInterrupt:
-        print('Post Failed')
+        print('Post Exit')
         print(KeyboardInterrupt)
