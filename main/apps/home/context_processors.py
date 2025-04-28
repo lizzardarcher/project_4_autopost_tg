274225d8df5caf1718d2525d767ed11512ccfea5
ansel <populator@live.com>
2025-04-28 10:46:16 +0500

    Исправления по логике

--- main/apps/home/context_processors.py
+++ main/apps/home/context_processors.py
@@ -1,53 +1,6 @@
 from .models import *
 
 
-# def user_settings(request):
-#
-#     try:
-#         settings = UserSettings.objects.filter(user_id=request.user.id)[0]
-#     except IndexError:
-#         settings = None
-#
-#     try:
-#         is_running = UserSettings.objects.filter(user_id=request.user.id)[0].bot_selected.is_started
-#         day = UserSettings.objects.filter(user_id=request.user.id)[0].bot_selected.day
-#     except IndexError:
-#         is_running = None
-#         day = None
-#
-#     try:
-#         return {
-#             'settings': settings,
-#             'is_running': is_running,
-#             'day': day,
-#             'bot_name_1': Bot.objects.filter(id=1).first().title,
-#             'bot_name_2': Bot.objects.filter(id=2).first().title,
-#             'bot_name_3': Bot.objects.filter(id=3).first().title,
-#             'bot_name_4': Bot.objects.filter(id=4).first().title,
-#             'bot_name_5': Bot.objects.filter(id=7).first().title,
-#             'bot_name_6': Bot.objects.filter(id=8).first().title,
-#             'bot_name_7': Bot.objects.filter(id=10).first().title,
-#             'bot_name_8': Bot.objects.filter(id=11).first().title,
-#             # 'bot_name_9': Bot.objects.filter(id=12).first().title,
-#             # 'bot_name_10': Bot.objects.filter(pk=13).first().title,
-#             'bot_is_active_1': Bot.objects.filter(pk=1).first().is_started,
-#             'bot_is_active_2': Bot.objects.filter(pk=2).first().is_started,
-#             'bot_is_active_3': Bot.objects.filter(pk=3).first().is_started,
-#             'bot_is_active_4': Bot.objects.filter(pk=4).first().is_started,
-#             'bot_is_active_5': Bot.objects.filter(pk=7).first().is_started,
-#             'bot_is_active_6': Bot.objects.filter(pk=8).first().is_started,
-#             'bot_is_active_7': Bot.objects.filter(pk=10).first().is_started,
-#             'bot_is_active_8': Bot.objects.filter(pk=11).first().is_started,
-#             # 'bot_is_active_9': Bot.objects.filter(pk=12).first().is_started,
-#             # 'bot_is_active_10': Bot.objects.filter(pk=13).first().is_started,
-#             'bot_selected': UserSettings.objects.filter(user_id=request.user.id)[0].bot_selected,
-#         }
-#     except:
-#         return {
-#             'settings': settings,
-#         }
-
-
 def user_settings(request):
     """
     Возвращает словарь настроек пользователя для отображения в шаблоне.
--- main/apps/telegram_bot/chat_id_handler.py
+++ main/apps/telegram_bot/chat_id_handler.py
@@ -10,57 +10,36 @@ from database import DataBase
 
 
 def get_chat_id():
-    # bot_token = DataBase.get_bot()[0]
     bots = DataBase.get_bots()
-    # print(bots)
-    # print(chats, bot_token)
     for bot in bots:
         chats = DataBase.get_chats_for_post_by_bot(bot[4])
-        # print(chats)
         if chats:
-            # r = requests.get(f'https://api.telegram.org/bot{bot_token}/getUpdates?offset=-100').text
             url = f'https://api.telegram.org/bot{bot[0]}/getUpdates'
             r = requests.get(url).text
-            # print(bot_token)
-            # print(url)
             try:
                 for chat in chats:
                     if not chat[2]:
                         chat_reference = chat[0].replace('@', '')
                         chat_title = chat[3]
-                        # print(chat_reference, chat_title)
                         sleep(6)
                         data = json.loads(r)
                         if data['ok']:
-                            # chat_id = data['result'][0]['my_chat_member']['chat']['id']
-                            # print(data['ok'])
                             for _data in data['result']:
                                 try:
                                     chat_id = _data['message']['chat']['id']
-                                    # print(chat_id)
-
                                     try:
                                         chat_username = _data['message']['chat']['username']
                                     except:
                                         chat_username = 'No username'
-
                                     chat_title_json = _data['message']['chat']['title']
-                                    # print(chat_id, chat_username, 'https://t.me/'+chat_reference)
                                     if chat_title_json == chat_title:
-                                        # print('SUPER!!!', (chat_id, chat_title))
                                         DataBase.update_chat_id((chat_id, chat_title))
-                                    # print(chat_id, chat_title)
                                 except Exception as e:
                                     pass
-                                    # print(traceback.format_exc())
                         else:
                             print('data not ok')
             except Exception as e:
-                # pass
                 print(traceback.format_exc())
-
-
-
 
 if __name__ == '__main__':
     try:
@@ -69,28 +48,3 @@ if __name__ == '__main__':
             sleep(150)
     except KeyboardInterrupt:
         print(KeyboardInterrupt)
-
-# if data['ok']:
-#     # chat_id = data['result'][0]['my_chat_member']['chat']['id']
-#     print(data['ok'])
-#     for _data in data['result']:
-#         try:
-#             chat_id = _data['my_chat_member']['chat']['id']
-#             print(chat_id)
-#
-#             try:
-#                 chat_username = _data['my_chat_member']['chat']['username']
-#             except:
-#                 chat_username = 'No username'
-#
-#             chat_title_json = _data['my_chat_member']['chat']['title']
-#             print(chat_id, chat_username, 'https://t.me/' + chat_reference)
-#             if chat_title_json == chat_title:
-#                 print('SUPER!!!', (chat_id, chat_title))
-#                 DataBase.update_chat_id((chat_id, chat_title))
-#             print(chat_id, chat_title)
-#         except Exception as e:
-#             # pass
-#             print(traceback.format_exc())
-# else:
-#     print('data not ok')
